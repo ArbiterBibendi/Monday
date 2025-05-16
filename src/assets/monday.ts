4d3e76f8b78b04714614fa84f241cf96a9f30a37
Drake Scott <ArbiterBibendi@yahoo.com>
2025-05-15 23:07:43 -0500

    monday init

---
+++ src/assets/monday.ts
@@ -0,0 +1,16 @@
+const text: string[] = [
+	"There are people being forced out of their homes.", 
+	"There are 10 million Americans without jobs.", 
+	"One morning, the Chairman and CEO of AIG got on national television and said,", 
+	"We're in trouble. We need $130 Billion...", 
+	"TO STAY OPEN!", 
+	"And by the way...", 
+	"WE NEED IT BY MONDAY!", 
+	'WE NEED THE MONEY BY MONDAY!"', 
+	"This is a company regulated by the Stock Exchange, by the SEC, BY CONGRESS!", 
+	"By ALL the insurance regulators!", 
+	"The most REGULATED, largest insurance company on earth.", 
+	"That means to me, all the numbers they've been publishing were false."
+];
+
+export default text;
