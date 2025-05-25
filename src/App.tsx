86ce4c15cf0ba21e032a9ac71af33064ded27f6d
Drake Scott <ArbiterBibendi@yahoo.com>
2025-05-25 17:45:39 -0500

    bamonday

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,67 @@
-import { useEffect, useRef, useState } from 'react'
 import Nav from "./components/Nav.tsx"
-import mondayText from "./assets/monday.js";
 
 function App() {
 
-  const [currentLine, setCurrentLine] = useState(0);
-  const mondayTextRef = useRef<HTMLHeadingElement>(null);
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentLine((cur) => {
-        console.log(cur);
-        if (cur < mondayText.length - 1) {
-          return cur + 1;
-        }
-        return cur;
-      });
-    }, 2000);
-    return () => {
-      clearInterval(interval);
-    }
-  }, []);
   return (
     <>
       <Nav />
       <div id="main">
-        <h1 ref={mondayTextRef} id="mondayText">
-          {mondayText[currentLine]}
-        </h1>
+        <section id="about">
+          <h1>
+            Drake Scott
+          </h1>
+          <h2>
+            Fullstack Developer
+          </h2>
+          <div className="two-column-layout-right">
+            <img src="keyboard.png" alt="screenshot of coding" className="filter"/>
+            <div className="sectionbody">
+              <ul>
+                <li>A developer who is comfortable using <b>Next.js</b>, <b>React</b>, and <b>JavaScript</b>/<b>HTML</b>/<b>CSS</b>, but down to learn new technologies :D</li>
+                <li>Always excited to take on challenging projects that help me push my skills further.</li>
+                <li>An easygoing person who values effectiveness and is always looking for new ways to grow as a developer.</li>
+              </ul>
+            </div>
+          </div>
+        </section>
+        <section id="skills">
+          <h1>Skills</h1>
+          <div className="sectionbody">
+            - Next.js
+            <br />
+            - React
+            <br />
+            - Javascript
+            <br />
+            - HTML
+            <br />
+            - CSS
+            <br />
+            - Node.js
+            <br />
+            - C#
+            <br />
+            Also:
+            <br />
+            - Python
+            <br />
+            - C++
+            <br />
+            - Reverse Engineering
+          </div>
+        </section>
+        <section id="projects">
+          <h1>Projects</h1>
+          <div className="sectionbody">
+
+          </div>
+        </section>
+        <section id="contactme">
+          <h1>Contact Me</h1>
+          <div className="sectionbody">
+
+          </div>
+        </section>
       </div>
     </>
   )
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,11 +1,13 @@
 function Nav(): React.JSX.Element {
 	return (
 		<nav>
-			<div>About</div>
-			<div>Skills</div>
-			<div>Projects</div>
-			<div>Contact Me</div>
-			<div>Resume</div>
+			<img src="drake.gif"></img>
+			<div>
+				<a href="#about">About</a>
+				<a href="#skills">Skills</a>
+				<a href="#projects">Projects</a>
+				<a href="#contactme" id="cta">Contact Me</a>
+			</div>
 		</nav>
 	);
 }
