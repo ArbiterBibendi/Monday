57a3db1436c144a77a40c75c78594b7c89fd383f
Drake Scott <ArbiterBibendi@yahoo.com>
2025-05-25 13:02:51 -0500

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import Nav from "./components/Nav.tsx"
 import mondayText from "./assets/monday.js";
-import './App.css'
 
 function App() {
 
+  const [currentLine, setCurrentLine] = useState(0);
+  const mondayTextRef = useRef<HTMLHeadingElement>(null);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentLine((cur) => {
+        console.log(cur);
+        if (cur < mondayText.length - 1) {
+          return cur + 1;
+        }
+        return cur;
+      });
+    }, 2000);
+    return () => {
+      clearInterval(interval);
+    }
+  }, []);
   return (
     <>
-    {
-      mondayText.map((text) => {
-        return <h1>{text}</h1>
-      })
-    }
+      <Nav />
+      <div id="main">
+        <h1 ref={mondayTextRef} id="mondayText">
+          {mondayText[currentLine]}
+        </h1>
+      </div>
     </>
   )
 }
---
+++ src/components/Nav.tsx
@@ -0,0 +1,13 @@
+function Nav(): React.JSX.Element {
+	return (
+		<nav>
+			<div>About</div>
+			<div>Skills</div>
+			<div>Projects</div>
+			<div>Contact Me</div>
+			<div>Resume</div>
+		</nav>
+	);
+}
+
+export default Nav;
