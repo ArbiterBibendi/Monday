8b7b0b2a8b0d06bda92f73dcefd59620f67aa05d
Drake Scott <ArbiterBibendi@yahoo.com>
2025-05-25 18:41:32 -0500

    skills n stylin

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,13 @@ function App() {
       <div id="main">
         <section id="about">
           <h1>
-            Drake Scott
+            <a href="#contactme">Drake Scott</a>
           </h1>
           <h2>
             Fullstack Developer
           </h2>
           <div className="two-column-layout-right">
-            <img src="keyboard.png" alt="screenshot of coding" className="triple-shadow"/>
+            <img src="keyboard.png" alt="screenshot of coding" className="triple-shadow" />
             <div className="sectionbody">
               <ul>
                 <li>A developer who is comfortable using <b>Next.js</b>, <b>React</b>, and <b>JavaScript</b>/<b>HTML</b>/<b>CSS</b>, but down to learn any stack :D</li>
@@ -26,28 +26,25 @@ function App() {
         </section>
         <section id="skills">
           <h1>Skills</h1>
-          <div className="sectionbody">
-            - Next.js
-            <br />
-            - React
-            <br />
-            - Javascript
-            <br />
-            - HTML
-            <br />
-            - CSS
-            <br />
-            - Node.js
-            <br />
-            - C#
-            <br />
-            Also:
-            <br />
-            - Python
-            <br />
-            - C++
-            <br />
-            - Reverse Engineering
+          <div className="two-column-layout">
+            <div className="sectionbody">
+              <h3>Technologies I use daily for web development</h3>
+              <ul>
+                <li>Next.js</li>
+                <li>React</li>
+                <li>Javascript</li>
+                <li>HTML</li>
+                <li>CSS</li>
+              </ul>
+            </div>
+            <div className="sectionbody">
+              <h3>Ones I use less often for other software development</h3>
+              <ul>
+                <li>C#</li>
+                <li>Python</li>
+                <li>C++</li>
+              </ul>
+            </div>
           </div>
         </section>
         <section id="projects">
