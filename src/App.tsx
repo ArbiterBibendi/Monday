1d966ad1aa83a39a7429dc80d18caba3d287a3e2
Drake Scott <ArbiterBibendi@yahoo.com>
2025-05-28 21:28:06 -0500

    all the the links

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { useRef, useState } from "react";
 import Nav from "./components/Nav.tsx"
-import { DiJavascript, DiCss3, DiHtml5, DiReact, DiGit } from "react-icons/di"
-import { SiNextdotjs, SiCplusplus, SiPython } from "react-icons/si"
+import { DiJavascript, DiCss3, DiHtml5, DiReact, DiGit, DiGithubBadge } from "react-icons/di"
+import { SiNextdotjs, SiCplusplus, SiPython, SiThunderstore } from "react-icons/si"
 import { TbBrandCSharp } from "react-icons/tb";
+import { LuAppWindow } from "react-icons/lu";
+import { BsDiscord } from "react-icons/bs";
 
 function App() {
   const form = useRef<HTMLFormElement>(null);
@@ -17,10 +19,10 @@ function App() {
     const formData = new FormData(form.current);
     const object = Object.fromEntries(formData);
     const json = JSON.stringify(object);
-    
+
     if (!form.current?.checkValidity() && formSectionBody.current !== null) {
-        formSectionBody.current.style = "border-color: red";
-        return;
+      formSectionBody.current.style = "border-color: red";
+      return;
     }
     fetch("https://api.web3forms.com/submit", {
       method: "POST",
@@ -30,25 +32,25 @@ function App() {
       },
       body: json
     })
-    .then(response => {
-      if (!formSectionBody.current) {
-        formSectionBody.current = new HTMLDivElement();
-      }
-      if (response.status == 200) {
-        formSectionBody.current.style = "border-color: lime";
-        setButtonDisabled(true);
-      }
-      else {
-        formSectionBody.current.style = "border-color: red";
-      }
-      return response.json();
-    })
-    .then(responseJson => {
-      if (!formStatus.current) {
-        return;
-      }
-      formStatus.current.innerText = responseJson.message;
-    })
+      .then(response => {
+        if (!formSectionBody.current) {
+          formSectionBody.current = new HTMLDivElement();
+        }
+        if (response.status == 200) {
+          formSectionBody.current.style = "border-color: lime";
+          setButtonDisabled(true);
+        }
+        else {
+          formSectionBody.current.style = "border-color: red";
+        }
+        return response.json();
+      })
+      .then(responseJson => {
+        if (!formStatus.current) {
+          return;
+        }
+        formStatus.current.innerText = responseJson.message;
+      })
   }
   return (
     <>
@@ -102,37 +104,54 @@ function App() {
           <div className="sectionbody">
             <div className="two-column-layout">
               <div className="projectcard">
-                <div>
+                <div className="desc">
+                  <h3>BendiTrivia</h3>
+                  <p>A trivia-hosting platform with user authentication, session-based rate limiting, and a moderation system.</p><br />
+                  <p>Uses Next.js, PostgreSQL, LuciaAuth, and Redis.</p><br/>
+                  <p className="hint">Demo Username: testuser</p>
+                  <p className="hint">Demo Password: testpassword</p>
+                </div>
+                <div className="projectlinks">
+                  <a target="_blank" href="https://github.com/ArbiterBibendi/BendiTrivia"><DiGithubBadge size={30} /></a>
+                  <a target="_blank" href="https://bendi-trivia.vercel.app/"><LuAppWindow size={28} /></a>
 
-                <h3>BendiTrivia</h3>
-                <p>A trivia-hosting platform with user authentication, session-based rate limiting, and a moderation system.</p><br/>
-                <p>Uses Next.js, PostgreSQL, LuciaAuth, and Redis.</p>
                 </div>
                 <img src="benditrivia2.png" alt="screenshot of benditrivia" />
               </div>
               <div className="projectcard">
-                <div>
-                <h3>Pateo</h3>
-                <p>A frontend designed to search for U.S. patents powered by the PatentsView API and React.</p>
+                <div className="desc">
+                  <h3>Pateo</h3>
+                  <p>A frontend designed to search for U.S. patents powered by the PatentsView API and React.</p>
+                </div>
+                <div className="projectlinks">
+                  <a target="_blank" href="https://github.com/ArbiterBibendi/pateo"><DiGithubBadge size={28} /></a>
                 </div>
                 <img src="pateo2.png" alt="screenshot of pateo" />
               </div>
               <div className="projectcard">
-                <div>
-                <h3>AEIOUCompany</h3>
-                <p>A mod for Lethal Company which implements Text-to-Speech into game chat.</p> <br/>
-                <p>Written in C# and uses HarmonyX for function hooking</p> <br/>
-                <p><b><i>540,000+</i></b> Downloads</p>
+                <div className="desc">
+                  <h3>AEIOUCompany</h3>
+                  <p>A mod for Lethal Company which implements proximity Text-to-Speech into game chat.</p> <br />
+                  <p>Written in C# and uses HarmonyX for function hooking</p> <br />
+                  <p><b><i>540,000+</i></b> Downloads</p>
+                </div>
+                <div className="projectlinks">
+                  <a target="_blank" href="https://github.com/ArbiterBibendi/AEIOUCompany"><DiGithubBadge size={28} /></a>
+                  <a target="_blank" href="https://thunderstore.io/c/lethal-company/p/Bibendi/AEIOUCompany/"><SiThunderstore id="thunderstoreicon"size={16}/></a>
+                  <a target="_blank" href="https://discord.gg/pzpAFhtGaj"><BsDiscord id="thunderstoreicon"size={16}/></a>
                 </div>
                 <img src="aeioucompany.png" alt="screenshot of aeioucompany page on thunderstore" />
               </div>
               <div className="projectcard">
-                <div>
-                <h3>Stalcraft Notifier</h3>
-                <p>A discord bot which which assists users with marketplace activity in Stalcraft.</p> <br/>
-                <p>Written in Javascript</p>
+                <div className="desc">
+                  <h3>Stalcraft Notifier</h3>
+                  <p>A discord bot which which assists users with marketplace activity in Stalcraft.</p> <br />
+                  <p>Written in Javascript</p>
                 </div>
-                <img src="stalcraftnotifier.png" alt="screenshot of the stalcraft notifier on discrd" />
+                <div className="projectlinks">
+                  <a target="_blank" href="https://github.com/ArbiterBibendi/Stalcraft"><DiGithubBadge size={28} /></a>
+                </div>
+                <img src="stalcraftnotifier2.png" alt="screenshot of the stalcraft notifier on discrd" />
               </div>
             </div>
           </div>
@@ -141,7 +160,7 @@ function App() {
           <h1>Contact Me</h1>
           <div className="sectionbody" ref={formSectionBody}>
             <form ref={form}>
-            <input type="hidden" name="access_key" value="5f7c3879-9bc7-4bf7-ada4-2e84fb49cbe8" />
+              <input type="hidden" name="access_key" value="5f7c3879-9bc7-4bf7-ada4-2e84fb49cbe8" />
               <label htmlFor="fname">Your name:</label>
               <input type="text" id="fname" name="fname" disabled={inputDisabled} required />
 
@@ -152,7 +171,7 @@ function App() {
               <textarea id="fmessage" name="fmessage" disabled={inputDisabled} required />
               <div>
                 <p ref={formStatus}></p>
-                <input type="submit" value="Send" className="cta" onClick={submitForm} disabled={inputDisabled}/>
+                <input type="submit" value="Send" className="cta" onClick={submitForm} disabled={inputDisabled} />
               </div>
             </form>
           </div>
